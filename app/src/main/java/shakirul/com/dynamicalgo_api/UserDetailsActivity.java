b9618314f5195f331ed2771fecbb89179860989f
shakirul15-311 <shakirul15-311@diu.edu.bd>
2019-05-28 14:45:13 +0600

    UI Design updated

--- app/src/main/java/shakirul/com/dynamicalgo_api/UserDetailsActivity.java
+++ app/src/main/java/shakirul/com/dynamicalgo_api/UserDetailsActivity.java
@@ -14,7 +14,7 @@ public class UserDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
-        getSupportActionBar().setTitle("Add User");
+        //getSupportActionBar().setTitle("Add User");
 
         BackBTN=findViewById(R.id.backBTN);
 
